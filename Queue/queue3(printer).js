33aebc84b5cf96b6daa7638ba78fa9912ac1a482
Abhijit694 <nayakabhijit543@gmail.com>
2026-01-25 23:11:14 +0530

    queue implementation of requests handled by a queue

---
+++ Queue/queue3(printer).js
@@ -0,0 +1,10 @@
+import Queue from "./queue1.js";
+
+const printerQueue = new Queue()
+printerQueue.enqueue("Document 1")
+printerQueue.enqueue("Document 2")
+printerQueue.enqueue("Document 3")
+
+while(!printerQueue.isEmpty()){
+    console.log("Printing",printerQueue.dequeue())
+}
