6b7699a843294ad761a300d3df078a11e050608b
Abhijit694 <nayakabhijit543@gmail.com>
2026-07-05 19:49:06 +0530

    correct logic to count digits using recursion

--- Basic math/Ques1(count_digits).js
+++ Basic math/Ques1(count_digits).js
@@ -17,11 +17,15 @@ function countDigits2(num){
 
 // With recursion
 function countDigits3(num){
-    num = Math.abs(num) // For negative numbers
-    
-    if(num < 10) return 1       // Base case
-    return countDigits3(Math.floor(num/10))     // Recursive case
+    if(num === 0) return 0
+
+    if(num < 0){            // for negative numbers
+        num = -(num)
+    }
+
+    if(num < 10) return 1
+    return 1 + countDigits3(Math.floor(num/10))
 }
 
 
-console.log(countDigits(7));
+console.log(countDigits3(-6473262));
